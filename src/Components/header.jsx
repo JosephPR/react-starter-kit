bc1ad36dcf5d9c9ae8a833dae320535f86f94995
JosephPR <reilly4181@gmail.com>
2020-04-12 23:24:29 -0600

    refactored components

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -12,12 +12,12 @@ function Header() {
         <img src="https://media1.giphy.com/media/LpiVeIRgrqVsZJpM5H/giphy.gif?cid=790b76114bfe2f8248992275c69ebe32306f265f484a4a81&rid=giphy.gif" className="App-logo" alt="logo" />
         <p>
          <ul>
-           <li><a href="https://reacttraining.com/react-router/">React Router</a></li>
-           <li><a href="https://material-ui.com/">Material Design</a></li>
-           <li><a href="https://github.com/axios/axios">Axios</a></li>
-           <li><a href="https://tailwindcss.com/">Tailwind</a></li>
-           <li><a href="https://www.npmjs.com/package/react-burger-menu">React Burger Menu</a></li>
-           <li><a href="https://github.com/JosephPR/react-starter-kit"> Repo link</a></li>
+           <li><a target="_blank" href="https://reacttraining.com/react-router/">React Router</a></li>
+           <li><a target="_blank" href="https://material-ui.com/">Material Design</a></li>
+           <li><a target="_blank" href="https://github.com/axios/axios">Axios</a></li>
+           <li><a target="_blank" href="https://tailwindcss.com/">Tailwind</a></li>
+           <li><a target="_blank" href="https://www.npmjs.com/package/react-burger-menu">React Burger Menu</a></li>
+           <li><a target="_blank" href="https://github.com/JosephPR/react-starter-kit"> Repo link</a></li>
          </ul>
         </p>
      
--- src/Components/three-column-grid.jsx
+++ src/Components/three-column-grid.jsx
@@ -10,12 +10,12 @@ function ThreeColumnGrid() {
    
       <h2>Github Resources</h2>
       <ul className="cards">
-        <li><a href="https://github.com/reactjs/reactjs.org">Reactjs</a></li>
-        <li><a href="https://github.com/taniarascia/react-tutorial">React tutorial</a></li>
-        <li><a href="https://github.com/facebook/react-native">React Native</a></li>
-        <li><a href="https://github.com/react-component">React Components</a></li>
-        <li><a href="https://github.com/ReactTraining">React Training</a></li>
-        <li><a href="https://github.com/ReactTraining/react-router/tree/master/packages/react-router-dom">React Router Dom</a></li>
+        <li><a target="_blank" href="https://github.com/reactjs/reactjs.org">Reactjs</a></li>
+        <li><a target="_blank" href="https://github.com/taniarascia/react-tutorial">React tutorial</a></li>
+        <li><a target="_blank" href="https://github.com/facebook/react-native">React Native</a></li>
+        <li><a target="_blank" href="https://github.com/react-component">React Components</a></li>
+        <li><a target="_blank" href="https://github.com/ReactTraining">React Training</a></li>
+        <li><a target="_blank" href="https://github.com/ReactTraining/react-router/tree/master/packages/react-router-dom">React Router Dom</a></li>
         
       </ul>
       </div>
